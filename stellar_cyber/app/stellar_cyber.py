2d063e4027ce41ffd3e0dd000ee9af4d1d9a23da
Sanket-solar <spadhy@securonix.com>
2026-04-09 03:11:12 +0530

    Soar 11793 (#34)
    * adding logger in stellar cyber to debug

--- stellar_cyber/app/stellar_cyber.py
+++ stellar_cyber/app/stellar_cyber.py
@@ -14,7 +14,9 @@ class StellarCyber():
     # Internal helpers
     # -------------------------------
     def _get_access_token(self, base_url, api_token, email=None):
+        self.logger.info("_get_access_token base url",base_url)
         url = f"{base_url}/connect/api/v1/access_token"
+        self.logger.info("Retrieving JWT token from %s", url)
         if email:
             resp = requests.post(url, auth=(email, api_token), timeout=30)
         else:
@@ -98,7 +100,7 @@ class StellarCyber():
             base_url = connectionParameters['base_url'].rstrip('/')
             api_token = connectionParameters['api_token']
             email = connectionParameters.get('email')
-            
+            self.logger.info("base_url & api_token",base_url,api_token)
             self._get_access_token(base_url, api_token, email)
             return {'status': 'success', 'message': 'Connected to Stellar Cyber successfully.'}
         except Exception as e:
